69b11d7a6e836685ca018d4fcc41295bdea74139
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2021-07-27 08:14:19 +0200

    Added Shopping Cart UI to Mango.Web

--- FrontEnd/Mango.Web/Services/CartService.cs
+++ FrontEnd/Mango.Web/Services/CartService.cs
@@ -28,7 +28,7 @@ namespace Mango.Web.Services
             });
         }
 
-        public async Task<T> GetCartByUserIdAsnyc<T>(string userId, string token = null)
+        public async Task<T> GetCartByUserIdAsync<T>(string userId, string token = null)
         {
             return await this.SendAsync<T>(new ApiRequest()
             {
--- FrontEnd/Mango.Web/Services/IServices/ICartService.cs
+++ FrontEnd/Mango.Web/Services/IServices/ICartService.cs
@@ -8,7 +8,7 @@ namespace Mango.Web.Services.IServices
 {
     public interface ICartService
     {
-        Task<T> GetCartByUserIdAsnyc<T>(string userId, string token = null);
+        Task<T> GetCartByUserIdAsync<T>(string userId, string token = null);
         Task<T> AddToCartAsync<T>(CartDto cartDto, string token = null);
         Task<T> UpdateCartAsync<T>(CartDto cartDto, string token = null);
         Task<T> RemoveFromCartAsync<T>(int cartId, string token = null);
